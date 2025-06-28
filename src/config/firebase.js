3c0ad32c8ff79284470c39ba39df855ef21fca7d
Kadir Efe Yazılı <152311530+KadirEfeYazili@users.noreply.github.com>
2025-06-29 00:17:26 +0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,17 +3,17 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAm8zrsQ82bx6a0lTEHy_CEnw73sHSGrO8",
-  authDomain: "yds-assistant.firebaseapp.com",
-  projectId: "yds-assistant",
-  storageBucket: "yds-assistant.firebasestorage.app",
-  messagingSenderId: "1082570080185",
-  appId: "1:1082570080185:web:d0d848f38c98cf2cfa8068",
-  measurementId: "G-BCQ2KE55PR"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { app, auth, db };
+export { app, auth, db }; 
