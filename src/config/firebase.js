5ee04e0543caa61bbcf41c364ca88f43fdc36c12
Kadir Efe Yazılı <152311530+KadirEfeYazili@users.noreply.github.com>
2025-06-29 00:13:19 +0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,11 +2,18 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const appId = typeof __app_id !== 'undefined' ? __app_id : 'default-app-id';
-const firebaseConfig = typeof _firebase_config !== 'undefined' ? JSON.parse(_firebase_config) : {};
+const firebaseConfig = {
+  apiKey: "AIzaSyAm8zrsQ82bx6a0lTEHy_CEnw73sHSGrO8",
+  authDomain: "yds-assistant.firebaseapp.com",
+  projectId: "yds-assistant",
+  storageBucket: "yds-assistant.firebasestorage.app",
+  messagingSenderId: "1082570080185",
+  appId: "1:1082570080185:web:d0d848f38c98cf2cfa8068",
+  measurementId: "G-BCQ2KE55PR"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { app, auth, db, appId }; 
+export { app, auth, db };
