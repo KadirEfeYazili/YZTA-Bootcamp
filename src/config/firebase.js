69e2753ac756698217da2692369b304ebc03e454
Muhammet Berke Ağaya <163898105+mberke1597@users.noreply.github.com>
2025-07-17 22:08:34 +0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,16 @@
+// config/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAKSwjetkHMql81024nhoIj4Ka6W65AplM",
+  authDomain: "yzta-akademi.firebaseapp.com",
+  projectId: "yzta-akademi",
+  storageBucket: "yzta-akademi.appspot.com",
+  messagingSenderId: "1033195492342",
+  appId: "1:1033195492342:web:8e7415ba995ba868d2edd7",
+  measurementId: "G-SSZB5BX7LN"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,4 +19,3 @@ const db = getFirestore(app);
 const appId = firebaseConfig.appId;
 
 export { app, auth, db, firebaseConfig, appId };
-
