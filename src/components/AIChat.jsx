d2d30894ff56ab9ed3f6923f2332d73dcb7e9979
Kadir Efe Yazılı <152311530+KadirEfeYazili@users.noreply.github.com>
2025-07-19 20:17:11 +0300

    Update AIChat.jsx
    avatar güncellendi

--- src/components/AIChat.jsx
+++ src/components/AIChat.jsx
@@ -53,7 +53,7 @@ const AIChat = ({ saveProgress }) => {
                         
                         {message.role === 'ai' && (
                             <img 
-                                src={`${process.env.PUBLIC_URL}/images/BaykusAvatar.png`}  // Avatar baykuş resmi eklendi
+                                src={`${process.env.PUBLIC_URL}/images/avatar.png`}  // Avatar resmi eklendi
                                 alt="AI asistanı avatarı"
                                 className="w-10 h-10 rounded-full flex-shrink-0 object-cover"
                             />
